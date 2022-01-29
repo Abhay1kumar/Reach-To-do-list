b1e64fe8d808f4cdecf563ffb31866f503b7ab97
Abhay1kumar <kumarabhay401@gmail.com>
2022-01-29 22:39:47 +0530

    Dog and Cat API

--- script.js
+++ script.js
@@ -1,33 +1,35 @@
-// const catbtn = document.querySelector('.cat-btn');
-const dogbtn= document.getElementById('dog-btn');
+
+const dog_btn=document.getElementById('dog-btn');
+const cat_btn=document.getElementById('cat-btn');
+
+const cat_img=document.getElementById('cat-img');
+const dog_img=document.getElementById('dog-img');
 
 
-// const catimg= document.querySelector('.cat-img');
-// const dogimg=document.querySelector('.dog-img');
 
-// dogbtn.addEventListener('click',()=>{
-//   fetch("https://dog.ceo/api/breeds/image/random")
-
-//   .then((response) =>response.json())
-//   .then((result) =>{
-//     console.log(result);
-//   })
-//   .catch((error) =>{
-//     console.log(error);
-//   })
-// 
-
-dogbtn.addEventListener('click',getDogImg)
-
-
-function getDogImg(){
+dog_btn.addEventListener('click', function(){
   fetch("https://dog.ceo/api/breeds/image/random")
+  .then((response) =>response.json())
+  .then((data) =>{
+    console.log(data.message);
+    dog_img.innerHTML = `<img src="${data.message}" width="300" height="280" alt="dog" />`;
+  })
+  .catch((error) =>{
+    console.log(error);
+  })
+})
 
-    .then((response) =>response.json())
-    .then((result) =>{
-      console.log(result);
-    })
-    .catch((error) =>{
-      console.log(error);
-    })
-}
+
+cat_btn.addEventListener('click', function(){
+  fetch("https://api.thecatapi.com/v1/images/search")
+  .then((response) =>response.json())
+  .then((data) =>{
+    console.log(data[0].url);
+    cat_img.innerHTML = `<img src="${data[0].url}" width="300" height="280" alt="cat" />`;
+  })
+  .catch((error) =>{
+    console.log(error);
+  })
+
+
+})
