be0934f602fe995644ae027e696bda93cde022da
Abhay1kumar <kumarabhay401@gmail.com>
2022-01-30 12:18:59 +0530

    India Covid Api

--- script.js
+++ script.js
@@ -1,39 +1,29 @@
-const name_person = document.getElementById('name')
-let person= prompt("Enter your name",);
-name_person.innerHTML=`<p>Hey!😀 ${person} thanks for coming here. lets click on buttons and see pictures.</p>`;
+let button = document.getElementById('btn');
+let result = document.getElementById('result');
+
+let url = "https://api.covid19api.com/summary";
+
+button.addEventListener('click', getData())
 
 
-const dog_btn=document.getElementById('dog-btn');
-const cat_btn=document.getElementById('cat-btn');
-
-const cat_img=document.getElementById('cat-img');
-const dog_img=document.getElementById('dog-img');
-
-
-
-dog_btn.addEventListener('click', function(){
-  fetch("https://dog.ceo/api/breeds/image/random")
-  .then((response) =>response.json())
-  .then((data) =>{
-    console.log(data.message);
-    dog_img.innerHTML = `<img src="${data.message}" width="300" height="280" alt="dog" />`;
-  })
-  .catch((error) =>{
-    console.log(error);
-  })
-})
-
-
-cat_btn.addEventListener('click', function(){
-  fetch("https://api.thecatapi.com/v1/images/search")
-  .then((response) =>response.json())
-  .then((data) =>{
-    console.log(data[0].url);
-    cat_img.innerHTML = `<img src="${data[0].url}" width="300" height="280" alt="cat" />`;
-  })
-  .catch((error) =>{
-    console.log(error);
-  })
+async function getData() {
+  try{
+  let data = await fetch(url)
+    let responce=await data.json();
+    let count=responce.Countries[76]
+    // console.log(responce.Countries[76])
+    result.innerHTML = `
+<p>Country Name: ${count.Country}</p>
+<p>Total Confirmed: ${count.TotalConfirmed}</p>
+<p>Total Death: ${count.TotalDeaths}</p>
+<p>Total Recovery: ${count.TotalRecovered}</p>
+<p>Data: ${count.Date}</p>
+`;
+  }catch(e){
+    console.log(e)
+  }
+ 
+}
+getData();
 
 
-})
